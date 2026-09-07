1d0384dcb31a3a9c8ac58f9852f501d7ca3d3f77
sampathreddyaleti127815-sketch <sampathreddy.aleti127815@marwadiuniversity.ac.in>
2026-09-07 13:02:01 +0530

    "first commit"

---
+++ Registration_Form_1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Registration
+{
+    public partial class Registration_From : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode=UnobtrusiveValidationMode.None;
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
